57a0f92ec559d5eb97d71ed54c278616d3725626
Megulo Abebe <meabebe@ucsd.edu>
2018-09-01 15:06:10 -0700

    testing conf mat

--- TRAIN/confusion_matrix.py
+++ TRAIN/confusion_matrix.py
@@ -24,23 +24,15 @@ validation_generator = test_datagen.flow_from_directory(
 class_indicies = validation_generator.class_indices
 class_list = list(class_indicies.keys())
 
-
-#Confution Matrix and Classification Report
+#Confusion Matrix
 Y_pred = model.predict_generator(validation_generator)
 y_pred = np.argmax(Y_pred, axis=1)
 cnf_mat = confusion_matrix(validation_generator.classes, y_pred)
-
 confusion_matrix_values = []
 for i in range(cnf_mat.shape[0]):
     confusion_matrix_values.append(cnf_mat[i])
 
-print('Classification Report')
-target_names = list(validation_generator.class_indices.keys())
-# print(classification_report(validation_generator.classes, y_pred, target_names=target_names))
-# print(class_list)
-# print(cnf_mat)
-
-# List of dictionaries
+# List of dictionaries for plotly
 dict_list = []
 for j in range(cnf_mat.shape[0]):
     for k in range(cnf_mat.shape[1]):
@@ -54,48 +46,6 @@ for j in range(cnf_mat.shape[0]):
 
         dict_list.append(dict_)
 
-# Save
-
-# Show the training
-# import matplotlib.pyplot as plt
-
-# Load in historical data
-# n = 0
-# train_acc = np.load('history_data/train_acc_{0}.npy'.format(n))
-# val_acc = np.load('history_data/val_acc_{0}.npy'.format(n))
-
-# train_loss = np.load('history_data/train_loss_{0}.npy'.format(n))
-# val_loss = np.load('history_data/val_loss_{0}.npy'.format(n))
-
-# # Plot Accuracy
-# num_iters = np.arange(len(train_acc))
-# plt.figure()
-# plt.plot(num_iters, train_acc, 'g', label='Training')
-# plt.plot(num_iters, val_acc, 'b', label='Validation')
-# plt.xlabel('Number of Iterations')
-# plt.ylabel('Accuracy (%)')
-# plt.title('Training set vs Validation')
-# plt.grid()
-# plt.legend()
-# # plt.show()
-
-# # Plot Loss
-# num_iters = np.arange(len(train_loss))
-# plt.figure()
-# plt.plot(num_iters, train_loss, 'g', label='Training')
-# plt.plot(num_iters, val_loss, 'b', label='Validation')
-# plt.xlabel('Number of Iterations')
-# plt.ylabel('Loss')
-# plt.title('Training set vs Validation')
-# plt.grid()
-# plt.legend()
-# plt.show()
-
-
-# Prettier confusion matrix
-import plotly.plotly as py
-from plotly.graph_objs import *
-py.sign_in('meguloabebe', 'P5vXd5GcQr07emsSeLul')
 trace = {
     "x":class_list,
     "y":class_list,
@@ -104,9 +54,17 @@ trace = {
     "showscale": True,
     "type": "heatmap"
 }
-data = Data([trace])
-layout = {
-    "annotations":dict_list
-}
-fig = Figure(data=data, layout=layout)
-plot_url = py.plot(fig)
+
+# Save Trace
+path_to_data_folder_in_webapp = os.getcwd()
+path_to_data_folder_in_webapp = path_to_data_folder_in_webapp.split('/')[:-1]
+path_to_data_folder_in_webapp = os.path.join(path_to_data_folder_in_webapp, 'web_app', 'static', 'data')
+import json
+trace_full_path = os.path.join(path_to_data_folder_in_webapp, 'trace.json')
+with open(trace_full_path, 'w') as trace_writer:
+    json.dump(trace, trace_writer)
+
+# Save data_list
+data_list_full_path = os.path.join(path_to_data_folder_in_webapp, 'data_list.json')
+with open(data_list_full_path, 'w') as data_writer:
+    json.dump(dict_list, data_writer)
