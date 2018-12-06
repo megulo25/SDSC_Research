bf6818180253ab94902ca1ce062255a888706b9e
Megulo Abebe <meabebe@ucsd.edu>
2018-12-06 14:27:14 -0800

    added class output to plot

--- TRAIN/multi-task/grad-cam.py
+++ TRAIN/multi-task/grad-cam.py
@@ -27,56 +27,81 @@ model = load_model(str(args['MODEL']))
 #----------------------------------------------------------------------#
 # Run Model
 y_pred = model.predict(x)
+y_leaf = y_pred[0]
+y_high = y_pred[1]
+
+class_dict = {
+        0: 'Goldeneye',
+        1: 'Grosbeak_Bunting',
+        2: 'Towhee',
+        3: 'Grebe',
+        4: 'Scaup_Duck',
+        5: 'Barrows_Goldeneye',
+        6: 'Blue_Grosbeak',
+        7: 'Clarks_Grebe',
+        8: 'Common_Goldeneye',
+        9: 'Eastern_Towhee',
+        10: 'Indigo_Bunting',
+        11: 'Lesser_Scaup',
+        12: 'Ring_Necked_Duck',
+        13: 'Spotted_Towhee',
+        14: 'Western_Grebe'
+}
+
+def get_class_name(dict_, y_leaf, y_high):
+    i=0
+    while True:
+        leaf = list(y_leaf[i])
+        high = list(y_high[i])
+        idx_leaf = leaf.index(1) + 5
+        idx_high = high.index(1)
+        i+=1
+        yield (dict_[idx_leaf], dict_[idx_high])
+gen = get_class_name(class_dict, y_leaf, y_high)
+
+y_leaf_label, y_high_label = next(gen)
 
 # Get Leaf and high outputs
 y_leaf_idx = np.argmax(y_pred[0])
-y_high_idx = np.argmax(y_pred[1])
 
 y_leaf_output = model.output[0][:, y_leaf_idx]
-y_high_output = model.output[1][:, y_high_idx]
 #----------------------------------------------------------------------#
 # Apply heatmap
 
 last_conv_layer = model.get_layer("block5_conv3")
 
 leaf_grads = K.gradients(y_leaf_output, last_conv_layer.output)[0]
-high_grads = K.gradients(y_high_output, last_conv_layer.output)[0]
 
 leaf_pool = K.mean(leaf_grads, axis=(0, 1, 2))
-high_pool = K.mean(high_grads, axis=(0, 1, 2))
 
 leaf_iterate = K.function([model.input], [leaf_pool, last_conv_layer.output[0]])
-high_iterate = K.function([model.input], [high_pool, last_conv_layer.output[0]])
 
 leaf_pool_value, leaf_conv_layer_value = leaf_iterate([x])
-high_pool_value, high_conv_layer_value = high_iterate([x])
 
 for i in range(512):
     leaf_conv_layer_value[:, :, i] *= leaf_pool_value[i]
-    high_conv_layer_value[:, :, i] *= high_pool_value[i]
 
 leaf_heatmap = np.mean(leaf_conv_layer_value, axis=-1)
 leaf_heatmap = np.maximum(leaf_heatmap, 0)
 leaf_heatmap /= np.max(leaf_heatmap)
-
-high_heatmap = np.mean(high_conv_layer_value, axis=-1)
-high_heatmap = np.maximum(high_heatmap, 0)
-high_heatmap /= np.max(high_heatmap)
-
 #----------------------------------------------------------------------#
 # Plot result
 img_old = mpimg.imread(img_path)
 img_old = cv2.resize(img_old, (224, 224))
 
 
-# leaf_heatmap = cv2.resize(leaf_heatmap, (img_old.shape[1], img_old.shape[0]))
-# leaf_heatmap = np.uint8(255 * leaf_heatmap)
-# leaf_heatmap = cv2.applyColorMap(leaf_heatmap, cv2.COLORMAP_JET)
-# leaf_superimposed_img = cv2.addWeighted(img_old, 0.6, leaf_heatmap, 0.4, 0)
+
+leaf_heatmap = cv2.resize(leaf_heatmap, (img_old.shape[1], img_old.shape[0]))
+leaf_heatmap = np.uint8(255 * leaf_heatmap)
+leaf_heatmap = cv2.applyColorMap(leaf_heatmap, cv2.COLORMAP_JET)
+leaf_superimposed_img = cv2.addWeighted(img_old, 0.6, leaf_heatmap, 0.4, 0)
 
 plt.subplot(1, 2, 1)
 plt.imshow(img_old)
+plt.title('True:\nHigh: {0}\nLeaf: {1}'.format(str('Goldeneye'), str('Barrows_Goldeneye')))
+
 
 plt.subplot(1, 2, 2)
-plt.imshow(high_superimposed_img)
+plt.imshow(leaf_superimposed_img)
+plt.title('Pred:\nHigh: {0}\nLeaf: {1}'.format(str(y_high_label), str(y_leaf_label)))
 plt.show()
